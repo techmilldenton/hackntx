58442f7870a8c2dfebd22093775cba7902153479
sffitzpatrick <sffitzpatrick@gmail.com>
2018-10-17 17:00:10 -0500

    update the param parser function to respond to any param and return various markup based on notification type

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,34 +1,57 @@
 // Custom.js file for rest of site.
+
+function getUrlParam ( prop ) {
+  var params = {};
+  var search = decodeURIComponent( window.location.href.slice( window.location.href.indexOf( '?' ) + 1 ) );
+  var definitions = search.split( '&' );
+
+  definitions.forEach( function( val, key ) {
+    var parts = val.split( '=', 2 );
+    params[ parts[ 0 ] ] = parts[ 1 ];
+  } );
+
+  return ( prop && prop in params ) ? params[ prop ] : null;
+};
+
+function generateAlert(message, severity) {
+  //TODO write the markup
+  return message;
+}
+
+function generateModal(message) {
+  //TODO write the markup
+  return message;
+}
+
+function parseNotifications() {
+  if (getUrlParam('notification').length) {
+    var param = getUrlParam('notification');
+    console.log(param);
+    $.getJSON( "assets/json/notifications.json", function( data ) {
+      $.each( data, function( key, notification ) {
+        if(key == param) {
+          var type = notification.type;
+          var message = notification.message;
+          var severity = notification.severity;
+
+          var formattedMessage = message
+
+          if( type == 'alert') {
+            formattedMessage = generateAlert(message, severity);
+          } else if( type == 'modal' ) {
+            formattedMessage = generateModal(message);
+          }
+          $('#off-canvas').append(formattedMessage);
+        }
+      });
+    });
+  }
+}
+
 $(document).ready(function(){
     // Target your .container, .wrapper, .post, etc.
     $(".video-container").fitVids();
 
-    function getUrlParam ( prop ) {
-    	var params = {};
-    	var search = decodeURIComponent( window.location.href.slice( window.location.href.indexOf( '?' ) + 1 ) );
-    	var definitions = search.split( '&' );
+    parseNotifications();
 
-    	definitions.forEach( function( val, key ) {
-    		var parts = val.split( '=', 2 );
-    		params[ parts[ 0 ] ] = parts[ 1 ];
-    	} );
-
-    	return ( prop && prop in params ) ? params[ prop ] : null;
-    };
-
-    // Show thank you on form submission.
-    if (getUrlParam("thankyou") !== null) {
-    	var thankYouContact = `<div class="alert alert-success">
-    	<div class="container">
-    	<div class="alert-icon">
-    	<i class="material-icons">check</i>
-    	</div>
-    	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    	<span aria-hidden="true"><i class="material-icons">clear</i></span>
-    	</button>
-    	<b>Success:</b> Thank you for your email! We'll get back to you shortly.
-    	</div>
-    	</div>`;
-    	$("main.main").prepend(thankYouContact);
-    }
 });
