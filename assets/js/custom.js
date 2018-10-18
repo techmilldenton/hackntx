08737b2bd394fe600751bbc7fa5eb2a1dd76bd9a
sffitzpatrick <sffitzpatrick@gmail.com>
2018-10-18 06:24:01 -0500

    clean up script, yml syntax, dom, styling

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,34 +14,61 @@ function getUrlParam ( prop ) {
 };
 
 function generateAlert(message, severity) {
-  //TODO write the markup
-  return message;
+  var alert = `<div class="alert alert-` + severity + `">
+                <div class="container">
+                  <div class="alert-icon">
+                    <i class="material-icons">check</i>
+                  </div>
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true"><i class="material-icons">clear</i></span>
+                  </button>
+                  <b>` + severity + `:</b> ` + message +`.
+                </div>
+              </div>`;
+  return alert;
 }
 
-function generateModal(message) {
-  //TODO write the markup
-  return message;
+function generateModal(title, message) {
+  var modal = `<div class="modal" id="notification-modal" tabindex="-1" role="dialog">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title">` + title + `</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        <p>` + message + `</p>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>`;
+  return modal;
 }
 
 function parseNotifications() {
-  if (getUrlParam('notification').length) {
+  if (getUrlParam('notification')) {
     var param = getUrlParam('notification');
-    console.log(param);
     $.getJSON( "assets/json/notifications.json", function( data ) {
       $.each( data, function( key, notification ) {
         if(key == param) {
           var type = notification.type;
           var message = notification.message;
-          var severity = notification.severity;
-
           var formattedMessage = message
-
           if( type == 'alert') {
+            var severity = notification.severity;
             formattedMessage = generateAlert(message, severity);
+            jQuery('main .main').prepend(formattedMessage);
           } else if( type == 'modal' ) {
-            formattedMessage = generateModal(message);
+            var title = notification.title;
+            formattedMessage = generateModal(title, message);
+            jQuery('main.main').after(formattedMessage);
+            jQuery('#notification-modal').modal();
           }
-          $('#off-canvas').append(formattedMessage);
         }
       });
     });
