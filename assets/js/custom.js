58f210c6f0e72f35a2bb0d132de83e6b4aedae5a
Kyle Taylor <kyletaylored@gmail.com>
2018-10-18 12:26:47 -0500

    Made a few changes
    - Made a common class for notifcations to target since the home page and other pages had different templates
    - Standardized contact success return param
    - Updated JS to use new common class

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,45 +15,46 @@ function getUrlParam ( prop ) {
 
 function generateAlert(message, severity) {
   var alert = `<div class="alert alert-` + severity + `">
-                <div class="container">
-                  <div class="alert-icon">
-                    <i class="material-icons">check</i>
-                  </div>
-                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true"><i class="material-icons">clear</i></span>
-                  </button>
-                  <b>` + severity + `:</b> ` + message +`.
-                </div>
-              </div>`;
+  <div class="container">
+  <div class="alert-icon">
+  <i class="material-icons">check</i>
+  </div>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+  <span aria-hidden="true"><i class="material-icons">clear</i></span>
+  </button>
+  <b>` + severity + `:</b> ` + message +`.
+  </div>
+  </div>`;
   return alert;
 }
 
 function generateModal(title, message) {
   var modal = `<div class="modal" id="notification-modal" tabindex="-1" role="dialog">
-                  <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title">` + title + `</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="modal-body">
-                        <p>` + message + `</p>
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>`;
+  <div class="modal-dialog" role="document">
+  <div class="modal-content">
+  <div class="modal-header">
+  <h5 class="modal-title">` + title + `</h5>
+  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+  <span aria-hidden="true">&times;</span>
+  </button>
+  </div>
+  <div class="modal-body">
+  <p>` + message + `</p>
+  </div>
+  <div class="modal-footer">
+  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+  </div>
+  </div>
+  </div>
+  </div>`;
   return modal;
 }
 
 function parseNotifications() {
   if (getUrlParam('notification')) {
     var param = getUrlParam('notification');
-    $.getJSON( "assets/json/notifications.json", function( data ) {
+    var $target = $(".notification-target");
+    $.getJSON( "/assets/json/notifications.json", function( data ) {
       $.each( data, function( key, notification ) {
         if(key == param) {
           var type = notification.type;
@@ -62,12 +63,12 @@ function parseNotifications() {
           if( type == 'alert') {
             var severity = notification.severity;
             formattedMessage = generateAlert(message, severity);
-            jQuery('main .main').prepend(formattedMessage);
+            $target.prepend(formattedMessage);
           } else if( type == 'modal' ) {
             var title = notification.title;
             formattedMessage = generateModal(title, message);
-            jQuery('main.main').after(formattedMessage);
-            jQuery('#notification-modal').modal();
+            $target.after(formattedMessage);
+            $('#notification-modal').modal();
           }
         }
       });
@@ -75,10 +76,13 @@ function parseNotifications() {
   }
 }
 
-$(document).ready(function(){
+(function($){
+  $(document).ready(function(){
     // Target your .container, .wrapper, .post, etc.
     $(".video-container").fitVids();
 
+    // Parse URL params for notifications.
     parseNotifications();
 
-});
+  });
+})(jQuery)
